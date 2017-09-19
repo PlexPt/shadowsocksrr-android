09f4f803b1cfc525a27a35217576a95e8f19e126
UzminTid <xunyanglins@gmail.com>
2017-09-19 18:22:23 +0800

    更新名称

--- src/main/scala/com/github/shadowsocks/Shadowsocks.scala
+++ src/main/scala/com/github/shadowsocks/Shadowsocks.scala
@@ -255,7 +255,7 @@ class Shadowsocks extends AppCompatActivity with ServiceBoundContext {
     setContentView(R.layout.layout_main)
     // Initialize Toolbar
     val toolbar = findViewById(R.id.toolbar).asInstanceOf[Toolbar]
-    toolbar.setTitle("shadowsocks R") // non-translatable logo
+    toolbar.setTitle("shadowsocks Rr") // non-translatable logo
     toolbar.setTitleTextAppearance(toolbar.getContext, R.style.Toolbar_Logo)
     val field = classOf[Toolbar].getDeclaredField("mTitleTextView")
     field.setAccessible(true)
